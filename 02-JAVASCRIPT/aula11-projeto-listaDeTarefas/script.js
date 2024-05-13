979b98d22d06a24afc2b4e406652e308fa081aaa
Nicolas Ferreira <nicolasferreiraleite@gmail.com>
2024-05-13 20:41:35 -0300

    projeto lista de tarefas finalizado

--- 02-JAVASCRIPT/aula11-projeto-listaDeTarefas/script.js
+++ 02-JAVASCRIPT/aula11-projeto-listaDeTarefas/script.js
@@ -2,7 +2,7 @@ let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || []; //desconverter de string para lista
 
 function renderTarefas(){
     listElement.innerHTML = "";
@@ -11,10 +11,24 @@ function renderTarefas(){
         let liElement = document.createElement("li"); //crio os itens da lista
         let tarefaText = document.createTextNode(todo); // crio o texto para cada item da lista
 
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode("🗑️");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
         liElement.appendChild(tarefaText); // mando o texto pro item da lista
+        liElement.appendChild(linkElement);
         listElement.appendChild(liElement); // manda o item da lista pra lista
     });
 }
+
+
+renderTarefas();
+
 
 function adicionarTarefas(){
     if(inputElement.value === ''){ // se ele n digitou nada 
@@ -25,7 +39,21 @@ function adicionarTarefas(){
         tarefas.push(novaTarefa); //puxo o input pra lista
         inputElement.value = ''; // limpo o input
         renderTarefas();
+        salvarDados();
     }
 }
 
 buttonElement.onclick = adicionarTarefas;
+
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1); // Deleta o item
+    renderTarefas();
+    salvarDados();
+}
+
+
+// salvando no local storage
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas)); // transformando a lista em string, pq so aceita em string
+}
