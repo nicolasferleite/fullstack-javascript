8f8a826e541e55a1e8c853949da79721631af5da
Nicolas Ferreira <nicolasferreiraleite@gmail.com>
2024-10-10 18:24:50 -0300

    configurando firebase

---
+++ 04-FIREBASE/aula01/src/App.js
@@ -0,0 +1,11 @@
+import { db } from './firebaseConnection';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ReactJS + Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-FIREBASE/aula01/src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore    } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAnUPiQES8tZwBO-cgTQRR7n3uFxeOY5I",
+    authDomain: "sujeito-programador-64d9c.firebaseapp.com",
+    projectId: "sujeito-programador-64d9c",
+    storageBucket: "sujeito-programador-64d9c.appspot.com",
+    messagingSenderId: "1066322657585",
+    appId: "1:1066322657585:web:7d64803cebbd2fa42bf010",
+    measurementId: "G-VNRB4RS2WC"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
